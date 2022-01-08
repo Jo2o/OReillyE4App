8aace8b084eb604f8803c82590025c861e099e76
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-09 00:44:01 +0100

    -- command, handler parameter

--- src/oreillye4app/handlers/HelloHandler.java
+++ src/oreillye4app/handlers/HelloHandler.java
@@ -1,6 +1,11 @@
 package oreillye4app.handlers;
 
+import javax.inject.Named;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +13,10 @@ public class HelloHandler {
     private static final Logger log = LoggerFactory.getLogger(HelloHandler.class);
 
     @Execute
-    public void hello() {
+    public void hello(
+            @Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
+            @Optional @Named("oreillye4app.commandparameter.hellovalue") String helloValue) {
         log.info("Hello from Handler!");
+        MessageDialog.openInformation(shell, "Hello", "Welcome . . .\n" + helloValue);
     }
 }
