ad05f44a021eb7d316add0a4a8cc22f833041a3d
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-08 22:51:36 +0100

    -- @Inject-ed MWindow

--- src/oreillye4app/parts/HelloPart.java
+++ src/oreillye4app/parts/HelloPart.java
@@ -1,7 +1,9 @@
 package oreillye4app.parts;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -9,15 +11,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloPart {
-
     private static final Logger log = LoggerFactory.getLogger(HelloPart.class);
+
+    @Inject
+    private MWindow window;
 
     private Label label;
 
     @PostConstruct
     public void create(Composite parent) {
         label = new Label(parent, SWT.NONE);
-        label.setText("Hello");
+        label.setText(window.getLabel());
         log.error("Hello {}", 5);
     }
 
