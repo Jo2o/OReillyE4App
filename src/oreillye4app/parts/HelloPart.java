3ceba06fee668c4a1e4c9b0de24a9f324909c0bc
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-09 00:05:21 +0100

    -- UISynchronize and Job

--- src/oreillye4app/parts/HelloPart.java
+++ src/oreillye4app/parts/HelloPart.java
@@ -2,11 +2,19 @@ package oreillye4app.parts;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.slf4j.Logger;
@@ -17,14 +25,18 @@ public class HelloPart {
 
     @Inject
     private MWindow window;
+    @Inject
+    private UISynchronize uiSynchronize;
 
     private Label label;
+    private Button button;
 
     @PostConstruct
     public void create(Composite parent) {
-        label = new Label(parent, SWT.NONE);
-        label.setText(window.getLabel());
-        log.error("Hello {}", 5);
+        createLabel(parent);
+        log.error("Label created...");
+        createButton(parent);
+        log.error("Button created...");
     }
 
     @Focus
@@ -36,6 +48,38 @@ public class HelloPart {
     @Optional
     public void receiveEvent(@UIEventTopic(RainbowPart.RAINBOW_COLOR) String data) {
         label.setText(data);
+    }
+
+    private void createLabel(Composite parent) {
+        label = new Label(parent, SWT.NONE);
+        label.setText(window.getLabel());
+    }
+
+    private void createButton(Composite parent) {
+        button = new Button(parent, SWT.NONE);
+        button.setText("DO NOT PUSH!");
+
+        button.addSelectionListener(new SelectionListener() {
+            @Override
+            public void widgetSelected(SelectionEvent event) {
+                button.setEnabled(false);
+
+                new Job("Button Push Job") {
+                    @Override
+                    protected IStatus run(IProgressMonitor progressMonitor) {
+                        // button.setEnabled(true); // this has to be run on UI thread!!!
+                        // => Display.getDefault().syncExec(() -> button.setEnabled(true));
+                        uiSynchronize.asyncExec(() -> button.setEnabled(true));
+                        return Status.OK_STATUS;
+                    }
+                }
+                .schedule(3000);
+
+            }
+            @Override
+            public void widgetDefaultSelected(SelectionEvent event) {
+            }
+        });
     }
 
     // @Inject
