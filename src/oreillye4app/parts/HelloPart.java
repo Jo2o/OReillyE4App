95beff34941105ad7abfccc32f1491403f7b99a3
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-08 22:22:39 +0100

    -- added slf4j log

--- src/oreillye4app/parts/HelloPart.java
+++ src/oreillye4app/parts/HelloPart.java
@@ -5,8 +5,12 @@ import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloPart {
+
+    private static final Logger log = LoggerFactory.getLogger(HelloPart.class);
 
     private Label label;
 
@@ -14,6 +18,7 @@ public class HelloPart {
     public void create(Composite parent) {
         label = new Label(parent, SWT.NONE);
         label.setText("Hello");
+        log.error("Hello {}", 5);
     }
 
     @Focus
