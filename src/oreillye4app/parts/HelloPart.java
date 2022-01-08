a2968f87f6e9840012507302bb4e8544c8591387
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2022-01-08 23:34:30 +0100

    -- events

--- src/oreillye4app/parts/HelloPart.java
+++ src/oreillye4app/parts/HelloPart.java
@@ -1,13 +1,11 @@
 package oreillye4app.parts;
 
-import static java.util.Objects.nonNull;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.inject.Named;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -36,9 +34,15 @@ public class HelloPart {
 
     @Inject
     @Optional
-    public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object selection) {
-        if (nonNull(selection)) {
-            label.setText(selection.toString());
-        }
+    public void receiveEvent(@UIEventTopic(RainbowPart.RAINBOW_COLOR) String data) {
+        label.setText(data);
     }
+
+    // @Inject
+    // @Optional
+    // public void setSelection(@Named(IServiceConstants.ACTIVE_SELECTION) Object selection) {
+    // if (nonNull(selection)) {
+    // label.setText(selection.toString());
+    // }
+    // }
 }
--- src/oreillye4app/parts/RainbowPart.java
+++ src/oreillye4app/parts/RainbowPart.java
@@ -2,23 +2,34 @@ package oreillye4app.parts;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 public class RainbowPart {
+    public static final String RAINBOW_COLOR = "rainbow/color";
+
     private static final String[] rainbowColors = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
 
+    // @Inject private ESelectionService selectionService;
+
     @Inject
-    private ESelectionService selectionService;
+    private IEventBroker eventBroker;
 
     @PostConstruct
     public void create(Composite parent) {
         ListViewer listViewer = new ListViewer(parent, SWT.NONE);
         listViewer.setContentProvider(new ArrayContentProvider());
         listViewer.setInput(rainbowColors);
-        listViewer.addSelectionChangedListener(event -> selectionService.setSelection(event.getSelection()));
+        // listViewer.addSelectionChangedListener(event->selectionService.setSelection(event.getSelection()));
+        listViewer.addSelectionChangedListener(this::setSelection);
+    }
+
+    public void setSelection(SelectionChangedEvent event) {
+        Object color = event.getStructuredSelection().getFirstElement();
+        eventBroker.post(RAINBOW_COLOR, color);
     }
 }
